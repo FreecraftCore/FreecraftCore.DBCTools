9404f94511ccc52231978c9e84b7f5c6bca38fe4
HelloKitty <andrew.blakely@ymail.com>
2018-06-16 12:43:31 -0500

    Added SpellChannelInterruptFlags from TC 3.3.5

---
+++ src/FreecraftCore.DBC.Common/Spell/SpellChannelInterruptFlags.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreecraftCore
+{
+	//TODO: What does TC mean by the below comment?
+	//From Trinitycore
+	// See SpellAuraInterruptFlags for other values definitions
+	/// <summary>
+	/// Enumeration of channeling interupt flags.
+	/// </summary>
+	[Flags]
+	public enum SpellChannelInterruptFlags : uint
+	{
+		CHANNEL_INTERRUPT_FLAG_INTERRUPT = 0x08,  // interrupt
+		CHANNEL_FLAG_DELAY = 0x4000
+	}
+}
