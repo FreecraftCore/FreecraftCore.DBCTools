60b5fb595d85bf5c9f221933c1daef2d075ab979
HelloKitty <andrew.blakely@ymail.com>
2018-06-17 16:49:17 -0500

    Added SpellRadius.dbc

---
+++ src/FreecraftCore.DBC.Structures/DBC/SpellRadiusEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using FreecraftCore.Serializer;
+using Newtonsoft.Json;
+
+namespace FreecraftCore
+{
+	[Table("SpellRadius")]
+	[JsonObject]
+	[WireDataContract]
+	public class SpellRadiusEntry : IDBCEntryIdentifiable
+	{
+		/// <inheritdoc />
+		[NotMapped]
+		[JsonIgnore]
+		public uint EntryId => SpellId;
+
+		//TODO: Is this spell id?
+		[WireMember(1)]
+		public uint SpellId { get; private set; }
+
+		[WireMember(2)]
+		public float Radius { get; private set; }
+
+		[WireMember(3)]
+		public int Zero { get; private set; }
+
+		[WireMember(4)]
+		public float Radius2 { get; private set; }
+
+		/// <inheritdoc />
+		public SpellRadiusEntry(uint spellId, float radius, int zero, float radius2)
+		{
+			SpellId = spellId;
+			Radius = radius;
+			Zero = zero;
+			Radius2 = radius2;
+		}
+
+		public SpellRadiusEntry()
+		{
+			
+		}
+	}
+}
