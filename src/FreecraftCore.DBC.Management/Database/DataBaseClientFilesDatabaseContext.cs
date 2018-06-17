b38bfd6fdff6c2026afac1d2d7ce297568fa5017
HelloKitty <andrew.blakely@ymail.com>
2018-06-16 20:13:33 -0500

    Added DBContext for DBC

---
+++ src/FreecraftCore.DBC.Management/Database/DataBaseClientFilesDatabaseContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreecraftCore
+{
+	public sealed class DataBaseClientFilesDatabaseContext : DbContext
+	{
+		/// <summary>
+		/// Represents the Spell.dbc table.
+		/// </summary>
+		public DbSet<SpellDBCEntry<string>> Spell { get; set; }
+
+		public DataBaseClientFilesDatabaseContext([NotNull] DbContextOptions options) 
+			: base(options)
+		{
+
+		}
+
+		//TODO: Remove this
+		/// <inheritdoc />
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseMySql("Server=localhost;Database=Client.DBC;Uid=root;Pwd=test;");
+
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		public DataBaseClientFilesDatabaseContext()
+		{
+			
+		}
+	}
+}
