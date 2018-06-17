258c1825952538d7f740ef287ac32d163cb2258b
HelloKitty <andrew.blakely@ymail.com>
2018-06-16 21:43:23 -0500

    Added simple tool to generate SQL table.

---
+++ src/FreecraftCore.DBC.CreateDatabase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FreecraftCore.DBC.CreateDatabase
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			Console.WriteLine("Creating table schema if not created.");
+			
+			DataBaseClientFilesDatabaseContext context = new DataBaseClientFilesDatabaseContext();
+
+			bool alreadyExists = !await context.Database.EnsureCreatedAsync();
+
+			Console.WriteLine($"Tables Already Existed: {alreadyExists}");
+			Console.WriteLine("Press any key!");
+
+			Console.ReadKey();
+		}
+	}
+}
