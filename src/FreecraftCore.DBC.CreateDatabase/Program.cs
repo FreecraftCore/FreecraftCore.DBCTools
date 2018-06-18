609d2ea89e66ac312f66736251a14df9fa03b68f
HelloKitty <andrew.blakely@ymail.com>
2018-06-17 22:13:58 -0500

    Added support for generic DBC models (TStringtype)

--- src/FreecraftCore.DBC.CreateDatabase/Program.cs
+++ src/FreecraftCore.DBC.CreateDatabase/Program.cs
@@ -54,21 +54,15 @@ namespace FreecraftCore
 
 			TypedParameter pathParameter = new TypedParameter(typeof(string), $"DBC/{dbcType}.dbc");
 			//TODO: Support configurable DBC location/path.
-			builder.RegisterType(typeof(GenericDatabaseDBCEntryInserter<>).MakeGenericType(dbcModelType))
-				.AsImplementedInterfaces()
-				.SingleInstance()
-				.WithParameter(pathParameter);
 
-			builder.RegisterType(typeof(GenericDbcFileEntryReader<>).MakeGenericType(dbcModelType))
-				.As(typeof(IDbcEntryReader<>).MakeGenericType(dbcModelType))
-				.SingleInstance()
-				.WithParameter(pathParameter);
+			//If it's an open generic model it will mean that it requires string type type args
+			//Not requiring this is much simplier, but it is still doable when the model is generic
+			if(!dbcModelType.IsGenericTypeDefinition)
+				RegisterNonGenericDbcModelServices(builder, dbcModelType, pathParameter);
+			else
+				RegisterGenericDbcModelServices(builder, dbcModelType, pathParameter);
 
-			builder.RegisterType(typeof(DbcDatabaseFileToTableConverter<>).MakeGenericType(dbcModelType))
-				.AsImplementedInterfaces()
-				.SingleInstance();
-
-			builder.RegisterType<DbcStringReader>()
+			builder.RegisterType<DbcFileStringReader>()
 				.As<IDbcStringReadable>()
 				.SingleInstance()
 				.WithParameter(pathParameter);
@@ -77,15 +71,15 @@ namespace FreecraftCore
 			//May look odd but some TypeConverters actually need the string database
 			//so they can convert from string offset/pointer to the actual string for the database.
 			builder.Register<IReadOnlyDictionary<uint, string>>(context =>
-				{
-					IDbcStringReadable readable = context.Resolve<IDbcStringReadable>();
+			{
+				IDbcStringReadable readable = context.Resolve<IDbcStringReadable>();
 
-					//This is blocking
-					return readable.ParseOnlyStrings()
-						.ConfigureAwait(false)
-						.GetAwaiter()
-						.GetResult();
-				})
+				//This is blocking
+				return readable.ParseOnlyStrings()
+					.ConfigureAwait(false)
+					.GetAwaiter()
+					.GetResult();
+			})
 				.SingleInstance()
 				.As<IReadOnlyDictionary<uint, string>>();
 
@@ -94,8 +88,85 @@ namespace FreecraftCore
 
 			//This takes the ASP/Core service collection and pushes it all into AutoFac.
 			builder.Populate(serviceCollection);
-			
+
 			return new AutofacServiceProvider(builder.Build());
+		}
+
+		/// <summary>
+		/// Similar to <see cref="RegisterNonGenericDbcModelServices"/> but handles the cases
+		/// where a TStringType generic type arg may need to be handled and dealt with.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <param name="dbcModelType"></param>
+		/// <param name="pathParameter"></param>
+		private static void RegisterGenericDbcModelServices([NotNull] ContainerBuilder builder, [NotNull] Type dbcModelType, [NotNull] TypedParameter pathParameter)
+		{
+			if(builder == null) throw new ArgumentNullException(nameof(builder));
+			if(dbcModelType == null) throw new ArgumentNullException(nameof(dbcModelType));
+			if(pathParameter == null) throw new ArgumentNullException(nameof(pathParameter));
+
+			GenericDbcModelAttribute genericAttri = dbcModelType.GetCustomAttribute<GenericDbcModelAttribute>();
+
+			if(genericAttri == null)
+				throw new InvalidOperationException($"Encountered generic Model: {dbcModelType.Name} that did not have {nameof(GenericDbcModelAttribute)} marked.");
+
+			//We can use the model attributes Types for the purposes of registeration
+			//and we trust that they are correct.
+			//We supply it he database model type because that is what the inserter should know about.
+			RegisterEntryInserter(builder, genericAttri.ClosedGenericForDatabase, pathParameter);
+			RegisterDbcFileReader(builder, genericAttri.ClosedGenericForFile, pathParameter);
+
+			//We actually use the DBC model file type here. This type has a dependency on an inserter
+			//of that generic parameter so it will find the above inserter does not work.
+			//To handle this we register a type specific to generic handlers
+			RegisterFileToDatabaseConverter(builder, genericAttri.ClosedGenericForFile);
+
+			//This will be the IDatabaseDbcInsertable<TDBCFileType> that the above converter looks for
+			//This itself will dispatch to the one registered in RegisterEntryInserter
+			builder.RegisterType(typeof(ConvertRequiredDatabaseEntryInserterDecorator<,>)
+					.MakeGenericType(genericAttri.ClosedGenericForFile, genericAttri.ClosedGenericForDatabase))
+				.AsImplementedInterfaces()
+				.SingleInstance();
+		}
+
+		private static void RegisterNonGenericDbcModelServices([NotNull] ContainerBuilder builder, [NotNull] Type dbcModelType, [NotNull] TypedParameter pathParameter)
+		{
+			if(builder == null) throw new ArgumentNullException(nameof(builder));
+			if(dbcModelType == null) throw new ArgumentNullException(nameof(dbcModelType));
+			if(pathParameter == null) throw new ArgumentNullException(nameof(pathParameter));
+
+			RegisterEntryInserter(builder, dbcModelType, pathParameter);
+			RegisterDbcFileReader(builder, dbcModelType, pathParameter);
+
+			//This is not a type converter, it converts to the SQL table.
+			RegisterFileToDatabaseConverter(builder, dbcModelType);
+		}
+
+		private static void RegisterFileToDatabaseConverter(ContainerBuilder builder, Type dbcModelType)
+		{
+			builder.RegisterType(typeof(DbcDatabaseFileToTableConverter<>).MakeGenericType(dbcModelType))
+				.AsImplementedInterfaces()
+				.SingleInstance();
+		}
+
+		private static void RegisterDbcFileReader(ContainerBuilder builder, Type dbcModelType, TypedParameter pathParameter)
+		{
+			builder.RegisterType(typeof(GenericDbcFileEntryReader<>).MakeGenericType(dbcModelType))
+							.As(typeof(IDbcEntryReader<>).MakeGenericType(dbcModelType))
+							.SingleInstance()
+							.WithParameter(pathParameter);
+		}
+
+		private static void RegisterEntryInserter([NotNull] ContainerBuilder builder, [NotNull] Type modelType, [NotNull] TypedParameter pathParameter)
+		{
+			if(builder == null) throw new ArgumentNullException(nameof(builder));
+			if(modelType == null) throw new ArgumentNullException(nameof(modelType));
+			if(pathParameter == null) throw new ArgumentNullException(nameof(pathParameter));
+
+			builder.RegisterType(typeof(GenericDatabaseDBCEntryInserter<>).MakeGenericType(modelType))
+				.AsImplementedInterfaces()
+				.SingleInstance()
+				.WithParameter(pathParameter);
 		}
 
 		//TODO: make this more configurable
---
+++ src/FreecraftCore.DBC.CreateDatabase/TypeConverters/DbcRefToStringTypeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace FreecraftCore
+{
+	/// <summary>
+	/// Abstract base for all generic string DBC models
+	/// that require conversions. They all need to convert strings or localized strings
+	/// so this simplfies the type creation.
+	/// </summary>
+	/// <typeparam name="TDbcFromType"></typeparam>
+	/// <typeparam name="TDbcToType"></typeparam>
+	public abstract class DbcRefToStringTypeConverter<TDbcFromType, TDbcToType> : ITypeConverterProvider<TDbcFromType, TDbcToType>
+		where TDbcFromType : IDBCEntryIdentifiable
+		where TDbcToType : IDBCEntryIdentifiable
+	{
+		protected ITypeConverterProvider<LocalizedStringDBC<StringDBCReference>, LocalizedStringDBC<string>> LocalizedStringConverter { get; }
+
+		protected ITypeConverterProvider<StringDBCReference, string> StringReferenceConverter { get; }
+
+		/// <inheritdoc />
+		protected DbcRefToStringTypeConverter([NotNull] ITypeConverterProvider<LocalizedStringDBC<StringDBCReference>, LocalizedStringDBC<string>> localizedStringConverter, [NotNull] ITypeConverterProvider<StringDBCReference, string> stringReferenceConverter)
+		{
+			LocalizedStringConverter = localizedStringConverter ?? throw new ArgumentNullException(nameof(localizedStringConverter));
+			StringReferenceConverter = stringReferenceConverter ?? throw new ArgumentNullException(nameof(stringReferenceConverter));
+		}
+		/// <inheritdoc />
+		public abstract TDbcToType Convert(TDbcFromType fromObject);
+	}
+}
