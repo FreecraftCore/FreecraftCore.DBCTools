51eac62157c8b7e2d07052056ac41cd056891644
HelloKitty <andrew.blakely@ymail.com>
2019-03-25 06:40:13 -0500

    Added build scripts

--- build_release.bat
+++ build_release.bat
@@ -3,3 +3,12 @@ dotnet restore FreecraftCore.DBCTools.sln
 dotnet publish src/FreecraftCore.DBC.CreateDBC/FreecraftCore.DBC.CreateDBC.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp2.1
 dotnet publish src/FreecraftCore.DBC.CreateDatabase/FreecraftCore.DBC.CreateDatabase.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp2.1
 dotnet publish src/FreecraftCore.DBC.CreateMPQ/FreecraftCore.DBC.CreateMPQ.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp2.1
+dotnet publish src/FreecraftCore.DBC.CreateJSON/FreecraftCore.DBC.CreateJSON.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp2.1
+PAUSE
+
+mkdir build
+mkdir build\release
+xcopy src\FreecraftCore.DBC.CreateDBC\Build build\release /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\FreecraftCore.DBC.CreateDatabase\Build build\release /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\FreecraftCore.DBC.CreateMPQ\Build build\release /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\FreecraftCore.DBC.CreateJSON\Build build\release /Y /q /EXCLUDE:BuildExclude.txt
