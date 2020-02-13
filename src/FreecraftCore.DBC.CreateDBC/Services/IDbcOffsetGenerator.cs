ec8f66495b58aebafbd8bca4c6d1fd1e3aad461a
HelloKitty <andrew.blakely@ymail.com>
2020-02-13 09:56:47 -0600

    Added Reset to IDbcOffsetGenerator interface

--- src/FreecraftCore.DBC.CreateDBC/Services/IDbcOffsetGenerator.cs
+++ src/FreecraftCore.DBC.CreateDBC/Services/IDbcOffsetGenerator.cs
@@ -14,5 +14,10 @@ namespace FreecraftCore
 		/// <param name="value">The string to generate an offset for.</param>
 		/// <returns>The offset for where the string will be.</returns>
 		uint CreateOffset([NotNull] string value);
+
+		/// <summary>
+		/// Resets the database.
+		/// </summary>
+		void Reset();
 	}
 }
