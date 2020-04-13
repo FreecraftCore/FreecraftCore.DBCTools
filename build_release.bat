e32e9e7a406a312dc573fb1ac6d54b0af455a872
HelloKitty <andrew.blakely@ymail.com>
2020-04-13 15:02:59 -0500

    Fixed release build script

--- build_release.bat
+++ build_release.bat
@@ -4,11 +4,11 @@ dotnet publish src/FreecraftCore.DBC.CreateDBC/FreecraftCore.DBC.CreateDBC.cspro
 dotnet publish src/FreecraftCore.DBC.CreateDatabase/FreecraftCore.DBC.CreateDatabase.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp3.1 /p:PublishSingleFile=true
 dotnet publish src/FreecraftCore.DBC.CreateMPQ/FreecraftCore.DBC.CreateMPQ.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp3.1 /p:PublishSingleFile=true
 dotnet publish src/FreecraftCore.DBC.CreateJSON/FreecraftCore.DBC.CreateJSON.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp3.1 /p:PublishSingleFile=true
-PAUSE
 
-mkdir build
-mkdir build\release
-xcopy src\FreecraftCore.DBC.CreateDBC\Build build\release /Y /q /EXCLUDE:BuildExclude.txt
-xcopy src\FreecraftCore.DBC.CreateDatabase\Build build\release /Y /q /EXCLUDE:BuildExclude.txt
-xcopy src\FreecraftCore.DBC.CreateMPQ\Build build\release /Y /q /EXCLUDE:BuildExclude.txt
-xcopy src\FreecraftCore.DBC.CreateJSON\Build build\release /Y /q /EXCLUDE:BuildExclude.txt
+::No idea why this happens, but it won't put it in RELEASE folder
+if not exist "build\release" mkdir build\release"
+xcopy src\FreecraftCore.DBC.CreateDBC\Build build\release\ /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\FreecraftCore.DBC.CreateDatabase\Build build\release\ /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\FreecraftCore.DBC.CreateMPQ\Build build\release\ /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\FreecraftCore.DBC.CreateJSON\Build build\release\ /Y /q /EXCLUDE:BuildExclude.txt
+copy lib\Stormlibsharp\StormLib.dll build /y
