4b90b439a33d7f465be907bd4e9be2e92284e950
HelloKitty <andrew.blakely@ymail.com>
2020-04-13 14:40:09 -0500

    Updated to netcore 3.x

--- build_release.bat
+++ build_release.bat
@@ -1,9 +1,9 @@
 dotnet restore FreecraftCore.DBCTools.sln
 
-dotnet publish src/FreecraftCore.DBC.CreateDBC/FreecraftCore.DBC.CreateDBC.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp2.1
-dotnet publish src/FreecraftCore.DBC.CreateDatabase/FreecraftCore.DBC.CreateDatabase.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp2.1
-dotnet publish src/FreecraftCore.DBC.CreateMPQ/FreecraftCore.DBC.CreateMPQ.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp2.1
-dotnet publish src/FreecraftCore.DBC.CreateJSON/FreecraftCore.DBC.CreateJSON.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp2.1
+dotnet publish src/FreecraftCore.DBC.CreateDBC/FreecraftCore.DBC.CreateDBC.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp3.1 /p:PublishSingleFile=true
+dotnet publish src/FreecraftCore.DBC.CreateDatabase/FreecraftCore.DBC.CreateDatabase.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp3.1 /p:PublishSingleFile=true
+dotnet publish src/FreecraftCore.DBC.CreateMPQ/FreecraftCore.DBC.CreateMPQ.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp3.1 /p:PublishSingleFile=true
+dotnet publish src/FreecraftCore.DBC.CreateJSON/FreecraftCore.DBC.CreateJSON.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp3.1 /p:PublishSingleFile=true
 PAUSE
 
 mkdir build
