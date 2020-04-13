4ab8c5c4f93fa61e20c71e2e5ef67da6de8b03db
HelloKitty <andrew.blakely@ymail.com>
2020-04-13 15:11:39 -0500

    Added stormlib license copy on build

--- build_release.bat
+++ build_release.bat
@@ -6,9 +6,11 @@ dotnet publish src/FreecraftCore.DBC.CreateMPQ/FreecraftCore.DBC.CreateMPQ.cspro
 dotnet publish src/FreecraftCore.DBC.CreateJSON/FreecraftCore.DBC.CreateJSON.csproj -c Release --self-contained -r win-x64 -o Build -f netcoreapp3.1 /p:PublishSingleFile=true
 
 ::No idea why this happens, but it won't put it in RELEASE folder
-if not exist "build\release" mkdir build\release"
+if not exist "build" mkdir build"
 xcopy src\FreecraftCore.DBC.CreateDBC\Build build\release\ /Y /q /EXCLUDE:BuildExclude.txt
 xcopy src\FreecraftCore.DBC.CreateDatabase\Build build\release\ /Y /q /EXCLUDE:BuildExclude.txt
 xcopy src\FreecraftCore.DBC.CreateMPQ\Build build\release\ /Y /q /EXCLUDE:BuildExclude.txt
 xcopy src\FreecraftCore.DBC.CreateJSON\Build build\release\ /Y /q /EXCLUDE:BuildExclude.txt
 copy lib\Stormlibsharp\StormLib.dll build /y
+copy lib\Stormlibsharp\stormlib_LICENSE.txt build /y
+copy lib\Stormlibsharp\stormlibsharp_LICENSE.txt build /y
