77e9c1af849f24562fa31c83822db7f1cdf50002
HelloKitty <andrew.blakely@ymail.com>
2020-04-22 20:00:20 -0500

    Fixed faction mod rates

---
+++ src/FreecraftCore.DBC.Management/Migrations/20200422221300_FixedFactionDBCModRateType.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreecraftCore.DBC.Management.Migrations
+{
+    public partial class FixedFactionDBCModRateType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "ParentFactionSpillOutMod",
+                table: "Faction",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "ParentFactionSpillInMod",
+                table: "Faction",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ParentFactionSpillOutMod",
+                table: "Faction",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ParentFactionSpillInMod",
+                table: "Faction",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float));
+        }
+    }
+}
