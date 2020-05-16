45b001c3e7e79f2a6642893db88621433ca781fc
HelloKitty <andrew.blakely@ymail.com>
2020-05-16 13:29:12 -0500

    Unified PrimaryKey Types

---
+++ src/FreecraftCore.DBC.Management/Migrations/20200516182749_UnifiedPrimaryKeyInt.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FreecraftCore.DBC.Management.Migrations
+{
+    public partial class UnifiedPrimaryKeyInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SpellRangeId",
+                table: "SpellRange",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpellRadiusId",
+                table: "SpellRadius",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProfanityNamesId",
+                table: "NamesProfanity",
+                nullable: false,
+                oldClrType: typeof(uint),
+                oldType: "int unsigned")
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<uint>(
+                name: "SpellRangeId",
+                table: "SpellRange",
+                type: "int unsigned",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "SpellRadiusId",
+                table: "SpellRadius",
+                type: "int unsigned",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.AlterColumn<uint>(
+                name: "ProfanityNamesId",
+                table: "NamesProfanity",
+                type: "int unsigned",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
